f7a4720880186b252756cd928fe6343bf4bee75c
Charlotte <cmjwfeltwell@gmail.com>
2026-04-06 16:52:29 -0700

    Update xcat_breathing.m

--- xcat_breathing.m
+++ xcat_breathing.m
@@ -1,7 +1,11 @@
-% Load XCAT samples
+% Load XCAT data
 data = load("data/sampling_300ms_compressed.mat");
 whos
 disp(fieldnames(data)) 
+N = 400; % phantom dimensions are 400x400
+NUM_SLICES = 50; % slices per sample
+NUM_SAMPLES = 100; 
+% data.data2(:,:,slice,sample)
 
 %%
 % Sample these frames (maybe a smaller subset - start with 10 of 100
@@ -11,28 +15,28 @@ for i=1:8 % for now let one breath be t=1 to 8
 end
 
 %% 
-num_breaths = 5;
-num_breathing_phases = 100; % change later
-num_spokes = 200;
 golden_angle = 111.246;
+num_breathing_phases = 100; % change later
 slice = 10;
 
-% should num spokes necessarily equal time for all breaths?
-% since not creating a breathing signal, no need for num breaths
-% should depend now on num spokes
+repetition_time = 0.004; % time to acquire 1 spoke [s]
+breaths = 5; 
+breath_cycle_time = 5; % time for one breath cycle [s]
+spokes = (breaths * breath_cycle_time) / repetition_time; 
 
-% sample projection
-sample_phantom = zeros(400,400);
+% sample projection to obtain sinogram dimensions
+sample_phantom = zeros(N,N);
 sample_projection = radon(sample_phantom, 0);
 
+% using radial MRI sampling
 angles = mod((0:num_spokes-1) * golden_angle, 180);
-%disp(['Num angles:' length(angles)]);
 sinogram = zeros(length(sample_projection), num_spokes);
 
 % 1 spoke per phase, probably unrealistic
-for i=1:num_spokes
-    idx = mod(i, num_breathing_phases) + 1; % 1-indexed
-    %disp(['Index ' num2str(idx)]);
+for i=1:spokes
+    %idx = mod(i, num_breathing_phases) + 1; % 1-indexed
+    idx = 1 + mod(floor(spokes / (breath_cycle_time/num_breathing_phases)), num_breathing_phases);
+    disp(num2str(idx));
     p = data.data2(:,:,1,idx);
     sino = radon(p, angles(i));
     sinogram(:,i) = sino;
