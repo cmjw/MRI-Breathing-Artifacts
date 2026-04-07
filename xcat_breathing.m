560e0bfac67748d9f25c13812c6a70441cdfaa77
Charlotte <cmjwfeltwell@gmail.com>
2026-04-06 21:24:10 -0700

    Params

--- xcat_breathing.m
+++ xcat_breathing.m
@@ -81,11 +81,11 @@ end
 
 %% Total Variation (TV) Regularization
 
-num_iters = 20; % 20-halo
-lambda = 0.005; % higher is smoother
-alpha = 0.005; % step size / learning rate
+num_iters = 30; % 20-halo
+lambda = 0.005 / length(bin_angles); % higher is smoother
+alpha = 0.05; % step size / learning rate
 epsilon = 1e-6; % to avoid division by 0
-tv_iters = 5;
+tv_iters = 0;
 
 % halo at i=20, alpha = 0.1.
 
@@ -136,7 +136,28 @@ for b=1:bins
 
         x(x<0) = 0; % constrain again
     end
+
+    % update step
     reconstructed_bins_tv(:,:,b) = x;
-    figure; imshow(reconstructed_bins_tv(:,:,b), []);
+
+    figure; subplot(1,3,1);
+    imshow(reconstructed_iradon(:,:,b), []);
+    title(['Bin ' num2str(b) ' (iradon)']);
+    subplot(1,3,2);
+    temp = ((b-1)*10 + 1) + floor((frames/bins)/2);
+    f = mod(temp, frames);
+    imshow(data.data2(:,:,slice,f), []);
+    title('Approximate Ground Truth');
+    subplot(1,3,3); imshow(reconstructed_bins_tv(:,:,b), []);
     title(['Bin ' num2str(b) ' (TV)']);
 end
+
+%% 
+figure;
+plot(1:num_iters, all_residuals, '-o', 'LineWidth', 2);
+grid on;
+xlabel('Iteration Number');
+ylabel('Relative Residual (Data Mismatch)');
+title('Convergence Plot of TV Reconstruction');
+legend_labels = arrayfun(@(x) sprintf('Bin %d', x), 1:bins, 'UniformOutput', false);
+legend(legend_labels);
